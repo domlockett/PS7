0b7fe7e98bc8c9f99a87e8d7b9af032023d45550
domlockett <domlockett@users.noreply.github.com>
2018-04-24 21:49:41 -0500

    Next step
    So, I've calculated all the appropriate vales and now I have to make the ggplots!

--- PS7_DL_R.R
+++ PS7_DL_R.R
@@ -1,24 +1,46 @@
 library("dplyr")
 setwd("C:/Users/Dominique/OneDrive/Documents/Spring 2018/Applied Statistical Programming/git/Week 14")
-
+library(ggplot2)
+attach(crime_stl)
 
 ##1
 
 crime_stl <- read.csv("C:/Users/Dominique/OneDrive/Documents/Spring 2018/Applied Statistical Programming/git/Week 14/March2018.CSV")
-
 View(crime_stl)
-
 ##2
+class(crime_stl$Description)
 
-attach(crime_stl)
-crime_stl<-mutate(crime_stl, Date=substr(DateOccur,1,10))
-crime_stl<-select(crime_stl, Date, District, Description, Neighborhood)
-crime_stl$Date<-as.Date(crime_stl$Date, "%m/%d/%Y")
-crime_stl$Description<-as.character(crime_stl$Description)
-class(crime_stl$Neighborhood)
-func<-function(x){ strsplit(x = x, split = "[-/]")[[1]][1]}
-crime_stl$Description<-lapply(crime_stl$Description, func)
-group_by(Description, Date)
-  summarise(count =n()) #summarize the count
+crime_dt <- crime_stl %>% 
+  mutate(DateOccur ,Date=substr(DateOccur,1,10)) %>%
+  mutate(Description, Crime = sapply(as.character(crime_stl$Description), function(x){strsplit(x = x, split = "[-/]")[[1]][1]})) %>%
+  group_by(Description) %>%
+  summarise(count =n()) 
+crime_dt$Crime
 
-class(crimeType_Day)
+most_crime<-arrange(crime_dt, desc(count))
+most_crime
+#Leaving the scene of a crime!
+
+##3
+
+crime_dn <- crime_stl %>% 
+  mutate(DateOccur ,Date=substr(DateOccur,1,10)) %>%
+  group_by(Crime, Neighborhood) %>%
+  summarise(count =n()) 
+neigh_crime<-arrange(crime_dn, desc(count))
+neigh_crime
+ ## Neighborhood 35
+
+crime_dn <- crime_stl %>% 
+  mutate(DateOccur ,Date=substr(DateOccur,1,10)) %>%
+  group_by(Crime, Neighborhood) %>%
+  summarise(count =n()) 
+neigh_crime<-arrange(crime_dn, desc(count))
+
+##4
+
+crime_rd <- data %>% 
+  group_by(District) %>% 
+  dplyr::filter(grepl('ROBBERY', Description)) %>% 
+  summarise (count = n()) %>%
+  mutate(freq = count / sum(count))
